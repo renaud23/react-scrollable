b0860d709d01ac918062df4216a45a2414982fb2
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-10-20 15:35:28 +0200

    up version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-export { default } from "./react-scrollable";
+export { default as ReactScrollable } from "./react-scrollable";
+export { default as ReactLargeTable } from "./react-large-table";
+export { default as ReactLargeTableEditable } from "./react-large-table-editable";
+export { default as ReactLargeList } from "./react-large-list";
+export { default as ReactLargeText } from "./react-large-text";
