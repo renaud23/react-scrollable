ba6f844839f4d766133055fe402ecc0974f3bc63
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-12-02 13:47:50 +0100

    on wheel

--- src/react-scrollable-ex/components/scrollable-container.js
+++ src/react-scrollable-ex/components/scrollable-container.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useCallback, useRef, useContext } from "react";
+import { getDeltaAmplitude } from "../../commons-scrollable";
+import { ScrollableContext, actions } from "../state-management";
 
 function ScrollableContainer({ children }) {
-  return <div className="react-scrollable-ex">{children}</div>;
+  const containerEl = useRef();
+  const [state, dispatch] = useContext(ScrollableContext);
+  const { vertical } = state;
+  const onWheel = useCallback(
+    function (e) {
+      e.preventDefault();
+      dispatch(actions.onVerticalScroll(getDeltaAmplitude(vertical, e.deltaY)));
+    },
+    [dispatch, vertical]
+  );
+
+  useEffect(
+    function () {
+      const { current } = containerEl;
+      if (current) {
+        current.addEventListener("wheel", onWheel, false);
+      }
+      return () => {
+        current.removeEventListener("wheel", onWheel);
+      };
+    },
+    [containerEl, onWheel]
+  );
+  return (
+    <div ref={containerEl} className="react-scrollable-ex">
+      {children}
+    </div>
+  );
 }
 
 export default ScrollableContainer;
