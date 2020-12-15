c7bd23bea159cdac22a71b0c40e15957fdf9b74d
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-12-15 08:56:59 +0100

    various

--- src/react-large-table/table-components/row-nums/row.js
+++ src/react-large-table/table-components/row-nums/row.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import classnames from "classnames";
 import { TableContext, actions } from "../../state-management";
-import { safeCss, Track } from "../../../commons-scrollable";
+import { safeCss } from "../../../commons-scrollable";
 import { useDispatch } from "../../commons-table";
 
 function Row({ height, index, rowNumRenderer: RowNum }) {
@@ -30,10 +30,9 @@ function Row({ height, index, rowNumRenderer: RowNum }) {
         odd: index % 2 === 1,
       })}
     >
-      <Track onTrack={onTrackCallback} horizontal bottom />
       <RowNum index={index} />
     </div>
   );
 }
 
-export default Row;
+export default React.memo(Row);
