3a002dceedbe4d24175461c5106bb17fa7050252
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-12-29 16:18:49 +0100

    drag and scroll

--- src/react-large-table/table-components/drag-and-drop-column.js
+++ src/react-large-table/table-components/drag-and-drop-column.js
@@ -1,11 +1,33 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useEffect, useState } from "react";
 import { actions } from "../state-management";
-import Dragger from "./dragger";
+import Dragger, { PORTAL_NAMES } from "./dragger";
 import { RowableContext } from "../../react-rowable/state-management";
 import { TableContext } from "../state-management";
 import { useDomEntities } from "../state-management";
 
+function Task({ delay = 250, activate }) {
+  useEffect(
+    function () {
+      let timer;
+      function timing() {
+        timer = window.setTimeout(function () {
+          activate();
+          timing();
+        }, delay);
+      }
+      timing();
+      return function () {
+        window.clearTimeout(timer);
+      };
+    },
+    [delay, activate]
+  );
+
+  return null;
+}
+
 function DragAndDropColumn() {
+  const [scrollRequest, setScrollRequest] = useState(undefined);
   const [state, dispatch] = useContext(TableContext);
   const { current: parent } = useContext(RowableContext)[2];
   const head = useDomEntities("th");
@@ -35,19 +57,43 @@ function DragAndDropColumn() {
     [head, draggedColumn, dispatch]
   );
 
+  const activate = useCallback(
+    function () {
+      dispatch(actions.onHorizontalScrollRequest({ ...scrollRequest }));
+    },
+    [scrollRequest, dispatch]
+  );
+
+  const onEnterPortal = function (direction) {
+    if (direction === PORTAL_NAMES.left) {
+      setScrollRequest({ delta: -1 });
+    } else if (direction === PORTAL_NAMES.right) {
+      setScrollRequest({ delta: 1 });
+    }
+  };
+
+  const onExitPortal = function () {
+    setScrollRequest(undefined);
+  };
+
   if (draggedColumn !== undefined) {
     const { clientX, clientY, label, node } = draggedColumn;
 
     return (
-      <Dragger
-        clientX={clientX}
-        clientY={clientY}
-        node={node}
-        parent={parent}
-        onClose={onClose}
-      >
-        <span className="th-dragger">{label}</span>
-      </Dragger>
+      <>
+        {scrollRequest ? <Task activate={activate} /> : null}
+        <Dragger
+          clientX={clientX}
+          clientY={clientY}
+          node={node}
+          parent={parent}
+          onClose={onClose}
+          onEnterPortal={onEnterPortal}
+          onExitPortal={onExitPortal}
+        >
+          <span className="th-dragger">{label}</span>
+        </Dragger>
+      </>
     );
   }
   return null;
